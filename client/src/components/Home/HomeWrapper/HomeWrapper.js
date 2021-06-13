88a7faef29acf00a70645c8a8c897799ad797311
Ben Lapidus <blapidus@u.rochester.edu>
2021-06-12 23:23:24 -0400

    Updated homepage and fixed window resize issue

--- client/src/components/Home/HomeWrapper/HomeWrapper.js
+++ client/src/components/Home/HomeWrapper/HomeWrapper.js
@@ -4,24 +4,32 @@ import "./HomeWrapper.scss";
 function HomeWrapper(props) {
 	const [link, setLink] = React.useState("");
 	const ytParser = (url) => {
-    var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/;
-    var match = url.match(regExp);
-    setLink((match&&match[7].length==11)? match[7] : false);
-}
+		var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/;
+		var match = url.match(regExp);
+		setLink(match && match[7].length == 11 ? match[7] : false);
+	};
 
 	return (
-		<div>
+		<div className="Home">
 			<h1 className="Home__header">Typerspace</h1>
 			<h2 className="Home__sub_header">Begin your mission</h2>
-
-			<div className="search-wrapper">
-			<form className="Home__form" action={"/dashboard"}>
-   				<input onChange={e => ytParser(e.target.value)} type="text" id="Home__ytlink" name="link" />
-   				<div className="Home__submitButtonWrapper">
-   				<input id="Home__submitButton" type="submit" value="take off"/>
-   				</div>
-  			</form>
-  			</div>
+			<div className="Homesearch-wrapper">
+				<form className="Home__form" action={"/dashboard"}>
+					<input
+						onChange={(e) => ytParser(e.target.value)}
+						type="text"
+						id="Home__ytlink"
+						name="link"
+					/>
+					<div className="Home__submitButtonWrapper">
+						<input
+							id="Home__submitButton"
+							type="submit"
+							value="take off"
+						/>
+					</div>
+				</form>
+			</div>
 		</div>
 	);
 }
