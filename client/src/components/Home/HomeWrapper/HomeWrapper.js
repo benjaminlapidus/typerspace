46a7156104fe239a1edbcb7a34f0c8037c1f3090
Ben Lapidus <blapidus@u.rochester.edu>
2021-06-12 22:59:22 -0400

    Updated homepage
    Removed bloat
    Changed global font

--- client/src/components/Home/HomeWrapper/HomeWrapper.js
+++ client/src/components/Home/HomeWrapper/HomeWrapper.js
@@ -3,7 +3,6 @@ import "./HomeWrapper.scss";
 
 function HomeWrapper(props) {
 	const [link, setLink] = React.useState("");
-
 	const ytParser = (url) => {
     var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/;
     var match = url.match(regExp);
@@ -12,33 +11,17 @@ function HomeWrapper(props) {
 
 	return (
 		<div>
-			<h1 className="header">Typerspace</h1>
-			<div className="solar-syst">
-				<div className="sun"></div>
-				<div className="mercury"></div>
-				<div className="venus"></div>
-				<div className="earth"></div>
-				<div className="mars"></div>
-				<div className="jupiter"></div>
-				<div className="saturn"></div>
-				<div className="uranus"></div>
-				<div className="neptune"></div>
-				<div className="asteroids-belt"></div>
-			</div>
+			<h1 className="Home__header">Typerspace</h1>
+			<h2 className="Home__sub_header">Begin your mission</h2>
 
 			<div className="search-wrapper">
-			<h2 className="bottom_header">Begin your mission</h2>
-			<form className="home__form" action={"/dashboard"}>
-   				<input onChange={e => ytParser(e.target.value)} type="text" id="ytlink" name="link" />
-   				<div className="submitButtonWrapper">
-   				<input id="submitButton" type="submit" value="takeoff"/>
+			<form className="Home__form" action={"/dashboard"}>
+   				<input onChange={e => ytParser(e.target.value)} type="text" id="Home__ytlink" name="link" />
+   				<div className="Home__submitButtonWrapper">
+   				<input id="Home__submitButton" type="submit" value="take off"/>
    				</div>
   			</form>
   			</div>
-
-			<div className="attribution">
-				Background created by <a href="https://github.com/KOWLOR">Malik Dellidj</a>
-			</div>
 		</div>
 	);
 }
