456874afdfa5ded3b05e5d3c347b9fbf1d16b261
Ben Lapidus <blapidus@u.rochester.edu>
2021-12-31 00:00:05 -0500

    Added tailwind and test component

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,9 @@
-import React from 'react';
-import MainComponent from './components/Main/Main/Main';
-import HomeComponent from './components/Home/Home/Home';
-import {BrowserRouter, Route, Link} from 'react-router-dom';
-import './App.css';
+import React from "react";
+import MainComponent from "./components/Main/Main/Main";
+import HomeComponent from "./components/Home/Home/Home";
+import { BrowserRouter, Route, Link } from "react-router-dom";
+import "./App.css";
+import "./index.css";
 
 class App extends React.Component {
   render() {
