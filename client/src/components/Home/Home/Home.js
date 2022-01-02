19c604eb55da3b68fbb4701910248a2e69969ead
Ben Lapidus <blapidus@u.rochester.edu>
2022-01-02 14:01:53 -0500

    Autocompile recompile tailwind and homepage updates

--- client/src/components/Home/Home/Home.js
+++ client/src/components/Home/Home/Home.js
@@ -14,85 +14,44 @@ const navigation = [
 
 function Home(props) {
   return (
-    <div class="container mx-auto max-w-sm">
-      <div className="shadow-lg px-4 py-6 w-full bg-white dark:bg-gray-800 relative">
-        <p className="text-sm w-max text-gray-700 dark:text-white font-semibold border-b border-gray-200">
-          Example component
-        </p>
-        <div className="flex items-end space-x-2 my-6">
-          <p className="text-5xl text-black dark:text-white font-bold">12</p>
-          <span className="text-green-500 text-xl font-bold flex items-center">
-            <svg
-              width="20"
-              fill="currentColor"
-              height="20"
-              className="h-3"
-              viewBox="0 0 1792 1792"
-              xmlns="http://www.w3.org/2000/svg"
+    <div className="relative bg-white dark:bg-gray-900 overflow-hidden">
+      <div className="max-w-7xl mx-auto">
+        <div className="relative h-screen z-10 pb-8 bg-white dark:bg-gray-900 sm:pb-16 md:pb-20  lg:w-full lg:pb-28 xl:pb-32">
+          <div className="relative pt-6 px-4 sm:px-6 lg:px-8">
+            <nav
+              className="relative flex items-center justify-between sm:h-10"
+              aria-label="Global"
             >
-              <path d="M1675 971q0 51-37 90l-75 75q-38 38-91 38-54 0-90-38l-294-293v704q0 52-37.5 84.5t-90.5 32.5h-128q-53 0-90.5-32.5t-37.5-84.5v-704l-294 293q-36 38-90 38t-90-38l-75-75q-38-38-38-90 0-53 38-91l651-651q35-37 90-37 54 0 91 37l651 651q37 39 37 91z"></path>
-            </svg>
-            22%
-          </span>
-        </div>
-        <div className="dark:text-white">
-          <div className="flex items-center pb-2 mb-2 text-sm space-x-12 md:space-x-24 justify-between border-b border-gray-200">
-            <p>Ben was here</p>
-            <div className="flex items-end text-xs">
-              34
-              <span className="flex items-center">
-                <svg
-                  width="20"
-                  fill="currentColor"
-                  height="20"
-                  className="h-3 text-green-500"
-                  viewBox="0 0 1792 1792"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path d="M1675 971q0 51-37 90l-75 75q-38 38-91 38-54 0-90-38l-294-293v704q0 52-37.5 84.5t-90.5 32.5h-128q-53 0-90.5-32.5t-37.5-84.5v-704l-294 293q-36 38-90 38t-90-38l-75-75q-38-38-38-90 0-53 38-91l651-651q35-37 90-37 54 0 91 37l651 651q37 39 37 91z"></path>
-                </svg>
-                22%
-              </span>
-            </div>
+              <div className="flex items-center flex-grow flex-shrink-0 lg:flex-grow-0">
+                <div className="flex items-center justify-between w-full md:w-auto">
+                  <a href="#">
+                    <span className="sr-only">Workflow</span>
+                    <img
+                      className="h-8 w-auto sm:h-10"
+                      src="https://tailwindui.com/img/logos/workflow-mark-purple-600.svg"
+                    />
+                  </a>
+                </div>
+              </div>
+              <div className="block md:ml-10 md:space-x-8 rounded bg-purple-800 hover:bg-purple-700 px-3 py-2 text-center text-gray-100">
+                <a href="#" className="font-medium">
+                  Log in
+                </a>
+              </div>
+            </nav>
           </div>
-          <div className="flex items-center mb-2 pb-2 text-sm space-x-12 md:space-x-24 justify-between border-b border-gray-200">
-            <p>And here</p>
-            <div className="flex items-end text-xs">
-              13
-              <span className="flex items-center">
-                <svg
-                  width="20"
-                  fill="currentColor"
-                  height="20"
-                  className="h-3 text-green-500"
-                  viewBox="0 0 1792 1792"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path d="M1675 971q0 51-37 90l-75 75q-38 38-91 38-54 0-90-38l-294-293v704q0 52-37.5 84.5t-90.5 32.5h-128q-53 0-90.5-32.5t-37.5-84.5v-704l-294 293q-36 38-90 38t-90-38l-75-75q-38-38-38-90 0-53 38-91l651-651q35-37 90-37 54 0 91 37l651 651q37 39 37 91z"></path>
-                </svg>
-                12%
-              </span>
+
+          <main className="grid grid-cols-12 gap-4 mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
+            <div className="col-start-2 col-span-10 sm:text-center">
+              <h1 className="text-6xl tracking-tight font-extrabold text-gray-900 dark:text-gray-100 sm:text-6xl md:text-9xl">
+                <span className="font-mono block xl:inline">typerspace</span>
+              </h1>
+              <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
+                Learning to type doesn't have to be boring. Practice while
+                typing alongside your favorite videos and songs.
+              </p>
             </div>
-          </div>
-          <div className="flex items-center text-sm space-x-12 md:space-x-24 justify-between">
-            <p>Not here</p>
-            <div className="flex items-end text-xs">
-              45
-              <span className="flex items-center">
-                <svg
-                  width="20"
-                  fill="currentColor"
-                  height="20"
-                  className="h-3 text-green-500"
-                  viewBox="0 0 1792 1792"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path d="M1675 971q0 51-37 90l-75 75q-38 38-91 38-54 0-90-38l-294-293v704q0 52-37.5 84.5t-90.5 32.5h-128q-53 0-90.5-32.5t-37.5-84.5v-704l-294 293q-36 38-90 38t-90-38l-75-75q-38-38-38-90 0-53 38-91l651-651q35-37 90-37 54 0 91 37l651 651q37 39 37 91z"></path>
-                </svg>
-                41%
-              </span>
-            </div>
-          </div>
+          </main>
         </div>
       </div>
     </div>
